bfadbaa2cc3e07b5f2bd5a7a27a4fee42f69896c
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2024-03-14 22:32:02 +0530

    Data cleaning function added

---
+++ text_utils.py
@@ -0,0 +1,7 @@
+import textwrap
+
+def wrap_text_preserve_newlines(text, width=110):
+    lines = text.split('\n')
+    wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
+    wrapped_text = '\n'.join(wrapped_lines)
+    return wrapped_text
