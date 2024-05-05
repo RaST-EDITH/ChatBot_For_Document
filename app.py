2e9eb96fd4bf18e5ed02d44a86ab333394849702
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2024-05-05 22:48:23 +0530

    Minor testing changes

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
+import time
 import os
 from dotenv import load_dotenv
 import textwrap
-from langchain import HuggingFaceHub
-# from langchain_community.llms import HuggingFaceHub
-from langchain.vectorstores import FAISS
-# from langchain_community.vectorstores import FAISS
-from langchain.document_loaders import TextLoader
-# from langchain_community.document_loaders import TextLoader
-from langchain.embeddings import HuggingFaceEmbeddings
-# from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain import HuggingFaceHub
+# from langchain.vectorstores import FAISS
+# from langchain.document_loaders import TextLoader
+# from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import HuggingFaceHub
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.question_answering import load_qa_chain
 
 load_dotenv()
 os.environ["HUGGINGFACEHUB_API_TOKEN"]=os.getenv("HUGGINGFACEHUB_API_TOKEN")
-loader = TextLoader("data.txt", encoding = "utf-8")
+loader = TextLoader(r"Dataset\data.txt", encoding = "utf-8")
 document = loader.load()
 
 def wrap_text_preserve_newlines( text, width=110 ) :
@@ -39,5 +40,8 @@ chain = load_qa_chain(llm, chain_type="stuff")
 
 queryText = "SRK son, daughter, wife name?"
 docsResult = db.similarity_search(queryText)
+print("Started")
+time.sleep(20)
+print("End")
 # print(chain.invoke(input_documents = docsResult, question = queryText))
 print(chain.run(input_documents = docsResult, question = queryText))
