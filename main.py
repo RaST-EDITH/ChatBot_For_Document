e7c09dbe4f75865219051b4b6dd5a6c51bc872ac
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2024-05-06 22:25:21 +0530

    Main function improvised

--- main.py
+++ main.py
@@ -11,18 +11,22 @@ import os
 """ This function provides a high-level interface for training or loading a model and vector store, 
 processing a query using the loaded model and vector store, and displaying the result."""
 
-# Check if the model and vector store are already trained and saved
-if not os.path.isfile(os.path.join( os.getcwd(), r"Processed_Data\qa_chain_model.joblib" )) or not os.path.isfile(os.path.join( os.getcwd(), r"Processed_Data\faiss_vector_store.joblib" )):
-    print("Training the model for the first time...")
-    train_and_save_model()
+def doc_chatbot(query_text) :
 
-# Load the model and vector store
-qa_chain = joblib.load(os.path.join( os.getcwd(), r"Processed_Data\qa_chain_model.joblib" ))
-vector_store = joblib.load(os.path.join( os.getcwd(), r"Processed_Data\faiss_vector_store.joblib" ))
+    # Check if the model and vector store are already trained and saved
+    if not os.path.isfile(os.path.join( os.getcwd(), r"Processed_Data\qa_chain_model.joblib" )) or not os.path.isfile(os.path.join( os.getcwd(), r"Processed_Data\faiss_vector_store.joblib" )):
+        # print("Training the model for the first time...")
+        train_and_save_model()
 
-# Query
-query_text = "SRK win how many awards and prizes in his career"
-result = process_query(query_text, qa_chain, vector_store)
+    # Load the model and vector store
+    print("Model Laoding Started->")
+    qa_chain = joblib.load(os.path.join( os.getcwd(), r"Processed_Data\qa_chain_model.joblib" ))
+    vector_store = joblib.load(os.path.join( os.getcwd(), r"Processed_Data\faiss_vector_store.joblib" ))
 
-# Display Result
-print(result)
+    # Query
+    result = process_query(query_text, qa_chain, vector_store)
+    return result
+
+if ( __name__ == '__main__' ) :
+    output = doc_chatbot("What is SRK full name")
+    print(output)
